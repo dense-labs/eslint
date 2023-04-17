e8f4ce3d378b8502b2356ce3a82becd2290a1128
DenseFog <move11@126.com>
2023-04-17 16:06:07 +0800

    pref: add eslint rule

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -12,7 +12,23 @@ module.exports = {
 	],
 	"plugins": ["@typescript-eslint", "prettier"],
 	"rules": {
-		"@typescript-eslint/no-explicit-any": 0,
+		// off
+		// handled by unused-imports/no-unused-imports
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/consistent-indexed-object-style': 'off',
+		'@typescript-eslint/naming-convention': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-member-accessibility': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/parameter-properties': 'off',
+		'@typescript-eslint/no-empty-interface': 'off',
+		'@typescript-eslint/ban-ts-ignore': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/triple-slash-reference': 'off',
+		'@typescript-eslint/no-this-alias': "off",
 		// prettier 作为规则
 		"prettier/prettier": ["error", {
 			"printWidth": 300,
@@ -20,6 +36,7 @@ module.exports = {
   			"jsxBracketSameLine": false,
 			"trailingComma": "none"
 		}],
+		'prefer-const': 'off',
 		// 禁止使用 var，而应该用 let 或 const
 		"no-var": "error"
 	}
